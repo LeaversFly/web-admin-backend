ad93900dd85a6d08eb57cfa567c78327f9e65dbb
Gin_99 <3348483879@qq.com>
2023-04-08 23:39:17 +0800

    修改取出数据库中时间数据的类型

--- src/models/file.ts
+++ src/models/file.ts
@@ -2,7 +2,7 @@ export default interface IFile {
     id: number,
     user_id: number,
     folder: string,
-    send_time: Date,
+    send_time: string,
     code: number,
     remain: number,
     validity: number
